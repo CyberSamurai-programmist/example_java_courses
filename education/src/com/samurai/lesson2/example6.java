f66cef837d8ad02b0e20f323f777a03435e74087
CyberSamurai <mathewsloban@yandex.com>
2021-04-19 23:12:37 +0300

    Completed all tasks from the Lesson 2

---
+++ education/src/com/samurai/lesson2/example6.java
@@ -0,0 +1,17 @@
+package com.samurai.lesson2;
+
+/**
+ * Даны 2 числа. Вывести большее из них
+ */
+public class example6 {
+    public static void main(String[] args) {
+        int a = 32;
+        int b = 26;
+
+        if (a > b) {
+            System.out.println("Большее число " + a);
+        } else {
+            System.out.println("Большее число " + b);
+        }
+    }
+}
